9fd9653ea5045471da99b55a475570e2feadf361
Sam Besse <sam.besse@gmail.com>
2025-08-26 22:31:46 -0400

    adding cJSON to begin parsing JSON for simple CAN emulation. adding initialization to can interfaces

--- c_can_emulator.c
+++ c_can_emulator.c
@@ -6,6 +6,16 @@
 #include <time.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <string.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "cJSON.h"
 /* general plan: 
  *  read message descriptors from a json file. Each message descriptor should have 
  *      a periodicity, or 0 if only on change
@@ -49,9 +59,15 @@ struct message_desc {
     pthread_mutex_t *lock;
 };
 
-
+int can_fds[4];
+void init_interfaces(int *fds);
+void send_message(union sigval s);
 
 int main(int argc, char **argv) {
+    // get the array of message descs and signal descs from the json file.
+    // initialize all can interfaces
+    // start a timer for each message descriptor
+    // start a terminal for accepting commands to change signals
     
 }
 
@@ -62,3 +78,29 @@ void send_message(union sigval s) {
     pthread_mutex_unlock(dsc->lock);
 }
 
+void init_interfaces(int *fds) {
+    char *can_names[] = {"can0", "can1", "can2", "can3"};
+    for (int i = 0; i < 4; i++) {
+        fds[i] = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+        if (fds[i] < 0) {
+            perror("failed to create socket");
+            continue;
+        }
+        struct ifreq ifr;
+        strcpy(ifr.ifr_name, can_names[i]);
+        ioctl(fds[i], SIOCGIFINDEX, &ifr);
+        struct sockaddr_can addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+        if (bind(fds[i], (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+            perror("failed finding socket");
+            fds[i] = -1;
+            continue;
+        }
+    }
+}
+
+int parse_can_json(char *filename) {
+    
+}
