2b7182d21b8fd66f2424d445b14bd5ec757b4c03
Sam Besse <sam.besse@gmail.com>
2025-08-28 22:48:48 -0400

    populating vector.h

--- vector.h
+++ vector.h
@@ -10,3 +10,7 @@ typedef struct vector {
 
 
 int vec_init(vec_t *vec, uint32_t element_size, uint32_t init_cap, uint32_t max_increment);
+int append(vec_t *vec, void *data);
+int pop(vec_t *vec, void *elem);
+int get(vec_t *vec, uint32_t index, void *element);
+int set(vec_t *vec, uint32_t index, void *element);
